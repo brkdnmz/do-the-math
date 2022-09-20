ab8ce43179f2761f1fe56b5c0894a888cc8145ee
Berke Dönmez <brkdnmz99@gmail.com>
2022-09-21 00:41:38 +0300

    Bugfix in AdminContext

--- src/context/AdminContext.tsx
+++ src/context/AdminContext.tsx
@@ -14,9 +14,12 @@ export default function AdminProvider({ children }: PropsWithChildren) {
   useEffect(() => {
     const checkKey = (e: KeyboardEvent) => {
       const pressedKey = e.key.toLowerCase();
+
       // Check if it is a letter -> length == 1, between "a" and "z"
       const isChar =
         pressedKey.length === 1 && "a" <= pressedKey && pressedKey <= "z";
+      if (!isChar) return;
+
       setAdminState((prevAdminState) => {
         const newState = { ...prevAdminState };
         let newKey = newState.enteredKey + pressedKey;
